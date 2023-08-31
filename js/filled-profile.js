0ddcedc0b6d63aa88dbc400e39e0a9cc0a45ebde
Tornike-Peradze <tornike.pheradze01@gmail.com>
2023-08-31 11:47:35 +0400

    changes in filled profile page

--- js/filled-profile.js
+++ js/filled-profile.js
@@ -1,36 +1,28 @@
 function updateFileName() {
   const inputElement = document.getElementById("upload-photo");
-  const labelElement = inputElement.nextElementSibling;
   const uploadPhotoWrapper = document.querySelector(".upload-photo");
 
   if (inputElement.files && inputElement.files[0]) {
     const reader = new FileReader();
 
     reader.onload = function (e) {
-      const uploadedImage = document.createElement("img");
-      uploadedImage.src = e.target.result;
-      uploadedImage.classList.add("uploaded-image");
-
-      // Remove any existing uploaded image
-      const existingImage = uploadPhotoWrapper.querySelector(".uploaded-image");
-      if (existingImage) {
-        existingImage.remove();
-      }
-
-      // Add the uploaded image to the wrapper
-      uploadPhotoWrapper.appendChild(uploadedImage);
-
-      // Hide the label
-      labelElement.style.display = "none";
+      const uploadedImageURL = e.target.result;
+      uploadPhotoWrapper.style.backgroundImage = `url('${uploadedImageURL}')`;
+      uploadPhotoWrapper.classList.add("upload-photo-background");
     };
 
     reader.readAsDataURL(inputElement.files[0]);
   } else {
-    // Remove the uploaded image and show the label
-    const existingImage = uploadPhotoWrapper.querySelector(".uploaded-image");
-    if (existingImage) {
-      existingImage.remove();
-    }
-    labelElement.style.display = "block";
+    uploadPhotoWrapper.style.backgroundImage =
+      "url('../assets/images/icon-upload-image.svg')";
+    uploadPhotoWrapper.classList.remove("upload-photo-background");
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const inputElement = document.getElementById("upload-photo");
+  const labelElement = inputElement.nextElementSibling;
+  const uploadPhotoWrapper = document.querySelector(".upload-photo");
+
+  inputElement.addEventListener("change", updateFileName);
+});
